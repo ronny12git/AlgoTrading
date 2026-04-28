7a4d627078d8cc204c068390ccf0c33bdce71404
ronny12git <wankhaderohan285@gmail.com>
2026-04-28 16:50:57 +0530

    fix: add /api to baseURL

--- AlgoTrading/frontend/src/services/api.js
+++ AlgoTrading/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://algotrading-orr6.onrender.com',
+  baseURL:'https://algotrading-orr6.onrender.com/api',
   timeout: 30000,
 })
 
